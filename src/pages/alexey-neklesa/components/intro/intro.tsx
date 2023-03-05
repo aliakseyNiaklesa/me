9a51fe67e0e208eb268885501d1c3fa0a0a8233c
Alexey Neklesa <alexey.neklesa@spiralscout.com>
2023-03-05 22:28:34 +0100

    Added intro

---
+++ src/pages/alexey-neklesa/components/intro/intro.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import styles from './styles.module.scss';
+
+export const Intro = () => {
+
+    return (
+        <section className={styles.intro}>
+            <span className={styles.name}>Alexey Neklesa</span>
+            <span className={styles.title}>Currently front-end developer at Spiral Scout</span>
+            <span className={styles.city}>Based in Poznań, Poland</span>
+            <span className={styles.resume}>
+                <a href="#">Download resume</a>
+                <a href="https://www.linkedin.com/in/alexeyneklesa/">Linkedn</a>
+            </span>
+            <span className={styles.bigTitle}>
+                <div>Front-end</div>
+                <div>developer with 6+</div>
+                <div>years of experience</div>
+            </span>
+        </section>
+    );
+}
---
+++ src/pages/alexey-neklesa/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Intro } from './components/intro';
+import './styles.scss';
+
+const AlexeyNekelesaCV = () => {
+    return (
+        <main>
+            <Intro />
+        </main>
+    )
+}
+
+export default AlexeyNekelesaCV;
