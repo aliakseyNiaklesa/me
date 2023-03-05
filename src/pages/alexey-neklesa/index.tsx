80a6c96ac61ca5ade5a7774ca0c40e6a383988ed
Alexey Neklesa <alexey.neklesa@spiralscout.com>
2023-03-05 23:08:24 +0100

    Added font approuch

--- src/pages/alexey-neklesa/index.tsx
+++ src/pages/alexey-neklesa/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] })
 
 import Intro from './components/intro';
 import './styles.scss';
 
 const AlexeyNekelesaCV = () => {
     return (
-        <main>
+        <main className={inter.className}>
             <Intro />
         </main>
     )
