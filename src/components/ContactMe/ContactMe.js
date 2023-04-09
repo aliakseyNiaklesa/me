eb66b4a38175ecce8ec7784388b94a8a966185e6
Alexey Neklesa <alexey.neklesa@spiralscout.com>
2023-04-09 22:00:33 +0200

    Removed components

--- src/components/ContactMe/ContactMe.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const ContactMe = () => {
-    return (
-        <section>
-            Contact me
-        </section>
-    );
-};
--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-export * from './Header';
-export * from './Title';
-export * from './Summary';
-export * from './Companies';
-export * from './AboutMe';
-export * from './ContactMe';
