1cce6b6c0a8d13c21867b32904c0d5bb2ca6120a
Alexey Neklesa <alexsanat@gmail.com>
2024-04-28 20:20:13 +0200

    code clean

--- src/pages/telegram-quiz/index.tsx
+++ src/pages/telegram-quiz/index.tsx
@@ -18,6 +18,7 @@ const TelegramQuiz: FC<TProps> = ({ tags }) => {
     return <main className={`${fonts.className}`}>
         {tags.map(tag => (
             <button
+                key={tag.id}
                 type="button"
                 className="w-full block py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
             >
